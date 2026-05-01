498ed95630f90e4ed060400f4899ba8fa215c5a4
ThakkarDivy11 <divythakkar318@gmail.com>
2026-05-01 14:41:46 +0530

    done

--- client/src/components/landing/GsapThreeHeroBackground.tsx
+++ client/src/components/landing/GsapThreeHeroBackground.tsx
@@ -74,9 +74,7 @@ const NeuralParticles = memo(({ isDark, count = 300 }: { isDark: boolean; count?
       <bufferGeometry>
         <bufferAttribute
           attach="attributes-position"
-          count={count}
-          array={positions}
-          itemSize={3}
+          args={[positions, 3]}
         />
       </bufferGeometry>
       <PointMaterial
